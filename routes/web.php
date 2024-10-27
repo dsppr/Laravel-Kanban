880d26028152b8809cc73b34eb43e03e67e84963
dsppr <by.dsppr@gmail.com>
2024-10-27 17:21:09 +0700

    Update all code in routes folder

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,6 @@ Route::get('/', function () {
     ]);
 });
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
