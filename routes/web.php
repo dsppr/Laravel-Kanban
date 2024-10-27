db621c89c91dc6e087a9ca3d53e1fa80fec6ce78
dsppr <by.dsppr@gmail.com>
2024-10-27 17:18:59 +0700

    Update code in web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,16 @@ Route::get('/', function () {
     ]);
 });
 
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
